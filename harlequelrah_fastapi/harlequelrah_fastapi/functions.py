b2317a83e9bc7d5caa33d4447ae62004755be14f
Harlequelrah <degbovimax19@gmail.com>
2024-09-24 20:23:40 +0000

    production du build pour harlequelrah_fastapi

--- harlequelrah_fastapi/harlequelrah_fastapi/functions.py
+++
@@ -1,8 +0,0 @@
-# retourne les attributs d"un model
-def attribut(MyModel):
-    model_attributes = [
-        attr
-        for attr in dir(MyModel)
-        if not callable(getattr(MyModel, attr)) and not attr.startswith("__")
-    ]
-    return model_attributes
