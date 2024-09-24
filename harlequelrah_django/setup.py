1603f22677d973eba0d6b83e95543621f1dc5d5b
Harlequelrah <degbovimax19@gmail.com>
2024-09-24 17:22:03 +0000

    correctif pour resoudre les importations

--- harlequelrah_django/setup.py
+++ harlequelrah_django/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="harlequelrah_django",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     description="Package personnalisé pour faciliter  le développement avec python avec django",
     long_description=open("README.md").read(),
