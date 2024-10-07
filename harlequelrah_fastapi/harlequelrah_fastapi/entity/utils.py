908fcc36447c53605838a4d41ff6d71f038db334
Harlequelrah <degbovimax19@gmail.com>
2024-10-07 00:21:15 +0000

    ameliorations de update_entity

--- harlequelrah_fastapi/harlequelrah_fastapi/entity/utils.py
+++ harlequelrah_fastapi/harlequelrah_fastapi/entity/utils.py
@@ -1,4 +1,4 @@
 def update_entity(existing_entity, update_entity):
     for key, value in update_entity.dict().items():
-        if value is not None:
+        if value is not None and hasattr(existing_entity, key):
             setattr(existing_entity, key, value)
